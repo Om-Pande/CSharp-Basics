6c35e0b6d0def04b62750a3b241a15b89cb8576f
Om-Pande <stud.200050116024@kpgu.ac.in>
2024-04-05 00:38:57 +0530

    Add project files.

---
+++ CSharp Basics/Variables.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+public class Variables
+{
+
+    public static void Main(String[] args)
+
+    {
+        // Category 1
+
+        byte A = byte.MaxValue;
+        int B = int.MaxValue;
+        long C = long.MaxValue;
+
+        // Category 2
+
+        float D = float.MaxValue;
+        double E = double.MaxValue;
+        char F = char.MaxValue;
+        bool G = true;
+        String H = "Perfect";
+
+
+        // Console log
+
+        Console.WriteLine(A);
+        Console.WriteLine(B);
+        Console.WriteLine(C);
+        Console.WriteLine(D);
+        Console.WriteLine(E);
+        Console.WriteLine(F);
+        Console.WriteLine(G);
+        Console.WriteLine(H);
+
+    }
+
+}
